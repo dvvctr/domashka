2c8d37f7fe95d960f107792c6fd7c5042ab24f28
Viktoriia <victoriadavidova07@gmail.com>
2024-06-16 21:38:25 +0300

    Add files via upload

---
+++ dz.py
@@ -0,0 +1,77 @@
+#task1
+
+# day = int(input("Enter number: "))
+# if day == 1:
+#    print("Monday")
+# elif day == 2:
+#    print("Tuesday")
+# elif day == 3:
+#    print("Wednesday")
+# elif day == 4:
+#    print("Thursday")
+# elif day == 5:
+#    print("Friday")
+# elif day == 6:
+#    print("Saturday")
+# elif day == 7:
+#    print("Sunday")
+# else: 
+#     print("Error")
+
+
+
+#task2
+
+# month = int(input("Enter number: "))
+# if month == 1:
+#    print("January")
+# elif month == 2:
+#    print("February")
+# elif month == 3:
+#    print("March")
+# elif month == 4:
+#    print("April")
+# elif month == 5:
+#    print("May")
+# elif month == 6:
+#    print("June")
+# elif month == 7:
+#    print("July")
+# elif month == 8:
+#    print("August")
+# elif month == 9:
+#    print("September")
+# elif month == 10:
+#    print("October")
+# elif month == 11:
+#    print("November")
+# elif month == 12:
+#    print("December")
+# else: 
+#     print("Error")
+
+
+
+#task3
+
+# num = int(input("Enter number: "))
+# if num > 0:
+#     print("Number is positive")
+# elif num < 0:
+#     print("Number is negative")
+# else:
+#     print("Number is equal to zero")
+
+
+
+#task4
+
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# if num1 == num2:
+#     print("numbers are equal")
+# elif num1 > num2:
+#     print(num2 , num1)
+# else:
+#     print(num1 , num2)
+
