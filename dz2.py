c1ccfaf5cb1fb27fed7ff2249e4910443119f6b1
Viktoriia <victoriadavidova07@gmail.com>
2024-06-16 22:54:09 +0300

    Add files via upload

---
+++ dz2.py
@@ -0,0 +1,45 @@
+#task 1
+
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# num3 = int(input("Enter the third number: "))
+# operation = int(input("Select operation 1 or 2: "))
+# if operation == 1:
+#     print(num1 + num2 + num3)
+# elif operation == 2:
+#     print(num1 * num2 * num3)
+# else:
+#     print("Error.")
+
+
+
+#task 2
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# num3 = int(input("Enter the third number: "))
+# operation = int(input("Select operation 1/2/3: "))
+# if operation == 1:
+#     result = max(num1, num2, num3)
+#     print("max:", result)
+# elif operation == 2:
+#     result = min(num1, num2, num3)
+#     print("min:", result)
+# elif operation == 3:
+#     result = (num1 + num2 + num3) / 3
+#     print("arithmetic mean:", result)
+# else:
+#     print("Error.")
+
+
+
+#task 3
+# meters = int(input("enter the number of meters: "))
+# operation = int(input("Select operations mile(1) , inch(2) or yard(3): "))
+# if operation == 1:
+#     print(meters * 0.00062137)
+# elif operation == 2:
+#     print(meters * 39.370)
+# elif operation == 3:
+#     print(meters * 1.0936)
+# else:
+#     print("Error.")
